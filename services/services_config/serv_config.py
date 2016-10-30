7f110427c8c98e2c771699eab25eaef62f4ea37f
Serhiy <mcseruy@gmail.com>
2016-10-30 18:55:00 +0200

    add config files

--- services/services_config/serv_config.py
+++ services/services_config/serv_config.py
@@ -8,3 +8,5 @@ config.read(os.path.join(path, 'serv_config.ini'))
 API_KEY = config.get('GOOGLE_API', 'api_key')
 CUSTOM_SEARCH_ENGINE_ID = config.get('GOOGLE_API', 'custom_search_engine_id')
 
+WIKI_URL = config.get('WIKI_PARSER', 'wiki_url')
+WIKI_DOGS_URL = config.get('WIKI_PARSER', 'wiki_dogs_url')
